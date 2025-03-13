5aeaf198371b69d896bca0550815eb469386efdf
Valher7 <Valentin.herman.pro@gmail.com>
2025-03-13 16:57:02 +0100

    add reset and delete features

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import {collection} from "./data/players.js";
 
+let allplayers = [... collection];
+let copie = [... collection];
+
 function createCards(position = "all") {
     
     const main = document.querySelector("main");
@@ -11,7 +14,7 @@ function createCards(position = "all") {
     const container = document.createElement("div");
     container.classList.add("players-container");
 
-    for(let player of collection) {
+    for(let player of copie) {
         const newSection = document.createElement("section");
         newSection.classList.add("player", player.position.toLowerCase());
 
@@ -27,6 +30,7 @@ function createCards(position = "all") {
         <p>${player.bio}</p>
         <hr>
         <a href="${player.instagram}" target="_blank"><img src="assets/instagram_logo_white.png" alt="Instagram Logo"></a>
+        <button type="button" id="${newSection.id}" class="button"><img src="assets/bin.png" alt="Bin Logo"></button>
         `;
         if (position === "all") {
             container.appendChild(newSection);
@@ -34,17 +38,32 @@ function createCards(position = "all") {
             continue;
         } else {
             container.appendChild(newSection);
-        }        
+        }     
     }
     main.appendChild(container);
+    document.querySelectorAll('button').forEach((button) => {
+        button.addEventListener('click', function() {
+            for(let player of copie) {
+                if(this.id.replace(/-/, " ") === player.name) {
+                    let index = copie.indexOf(player);
+                    let confirmation = confirm(`Are you sure you want to delete ${player.name} from your collection?`);
+                    if(confirmation === true){
+                        copie.splice(index, 1);
+                    createCards(document.getElementById("position").value);
+                    }           
+                }
+            }
+        });
+    });
     }
 createCards();
 
-
-const event = document.addEventListener("change", function () {
+const filter = document.addEventListener("change", function () {
     let position = document.getElementById("position").value;
     createCards(position);
-    })   
+    })  
 
-
-
+const reset = document.getElementById("reset").addEventListener("click", function() { 
+    copie = [... allplayers]; 
+    createCards(document.getElementById("position").value);       
+})
